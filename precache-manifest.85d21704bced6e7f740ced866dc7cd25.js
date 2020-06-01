05671a407823e6f7b802755af6c59ce0ad372783
Lev Vasilyev <lev@Levs-MacBook-Pro.local>
2020-06-01 06:30:36 +0300

    Updates

---
+++ precache-manifest.85d21704bced6e7f740ced866dc7cd25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b338bc78cbe05204c9445a24685c0d4",
+    "url": "/zoom-cc/index.html"
+  },
+  {
+    "revision": "7e73ef5263dc2acadc5c",
+    "url": "/zoom-cc/static/css/main.cc2ee3d0.chunk.css"
+  },
+  {
+    "revision": "f885c518bd05e766980a",
+    "url": "/zoom-cc/static/js/2.134f3416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zoom-cc/static/js/2.134f3416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e73ef5263dc2acadc5c",
+    "url": "/zoom-cc/static/js/main.1cdaaaf4.chunk.js"
+  },
+  {
+    "revision": "9848f1a4df2a3f8158be",
+    "url": "/zoom-cc/static/js/runtime-main.ef80c17f.js"
+  }
+]);
